8007b43946dd3d263eced1e5f7dce769ed2121c0
manudevelopia <manu@developia.info>
2021-11-26 17:49:25 +0100

    add validate object null implementation code

---
+++ src/main/java/info/developia/validate/Object.java
@@ -0,0 +1,18 @@
+package info.developia.validate;
+
+public class Object {
+
+    public static <T> T notNull(T object) {
+        if (object == null) {
+            throw new NullPointerException();
+        }
+        return object;
+    }
+
+    public static <T> T notNull(T object, String message) {
+        if (object == null) {
+            throw new NullPointerException(message);
+        }
+        return object;
+    }
+}
