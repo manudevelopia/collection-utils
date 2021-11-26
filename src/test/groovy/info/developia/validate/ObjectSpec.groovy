6df7d65f25968a88251e452f139cf37767d49786
manudevelopia <manu@developia.info>
2021-11-26 17:48:43 +0100

    add test to validate object null

---
+++ src/test/groovy/info/developia/validate/ObjectSpec.groovy
@@ -0,0 +1,39 @@
+package info.developia.validate
+
+import spock.lang.Specification
+
+class ObjectSpec extends Specification {
+
+    class MyObject {}
+
+    def "should return object"() {
+        given:
+        MyObject myObject = new MyObject()
+        when:
+        def result = Object.notNull(myObject)
+        then:
+        myObject == result
+    }
+
+    def "should throw a NullPointer exception"() {
+        given:
+        MyObject myObject = null
+        when:
+        Object.notNull(myObject)
+        then:
+        thrown(NullPointerException.class)
+    }
+
+    def "should throw a NullPointer exception with message #message"() {
+        given:
+        String message = "Oh, this shouldn't be null!!"
+        MyObject myObject = null
+        when:
+        Object.notNull(myObject, message)
+        then:
+        def exception = thrown(NullPointerException.class)
+        message == exception.getMessage()
+    }
+
+
+}
